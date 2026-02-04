d5a4349cd5815d2d59c9996b1546778a7e4fccef
Alfredo <alfredobautista1@gmail.com>
2026-02-04 20:01:55 +0100

    remove import

--- lib/profile/data/profile_repository.dart
+++ lib/profile/data/profile_repository.dart
@@ -1,5 +1,4 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutterconf/profile/models/user_profile.dart';
 
 class ProfileRepository {
