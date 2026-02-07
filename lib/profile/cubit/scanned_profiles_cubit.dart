757fe8c868b209ff85bb725b441c06e9be2b4dc7
Alfredo <alfredobautista1@gmail.com>
2026-02-07 19:11:10 +0100

    save if the scanned profile is a new scan

--- lib/profile/cubit/scanned_profiles_cubit.dart
+++ lib/profile/cubit/scanned_profiles_cubit.dart
@@ -1,5 +1,9 @@
+import 'dart:async';
+
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutterconf/auth/auth.dart';
+import 'package:flutterconf/profile/data/profile_repository.dart';
 import 'package:flutterconf/profile/data/scanned_profiles_repository.dart';
 import 'package:flutterconf/profile/models/scanned_profile.dart';
 
@@ -8,10 +12,16 @@ part 'scanned_profiles_state.dart';
 class ScannedProfilesCubit extends Cubit<ScannedProfilesState> {
   ScannedProfilesCubit({
     required ScannedProfilesRepository repository,
+    required ProfileRepository profileRepository,
+    required AuthRepository authRepository,
   }) : _repository = repository,
+       _profileRepository = profileRepository,
+       _authRepository = authRepository,
        super(const ScannedProfilesInitial());
 
   final ScannedProfilesRepository _repository;
+  final ProfileRepository _profileRepository;
+  final AuthRepository _authRepository;
 
   Future<void> loadProfiles() async {
     emit(const ScannedProfilesLoading());
@@ -25,8 +35,14 @@ class ScannedProfilesCubit extends Cubit<ScannedProfilesState> {
 
   Future<void> addProfile(ScannedProfile profile) async {
     try {
-      await _repository.addScannedProfile(profile);
+      final isProfileAdded = await _repository.addScannedProfile(profile);
       await loadProfiles();
+
+      // Sync with Firestore in background if it's a new scan
+      final userId = _authRepository.currentUser?.uid;
+      if (isProfileAdded && userId != null) {
+        unawaited(_profileRepository.incrementScannedCount(userId));
+      }
     } catch (_) {
       emit(const ScannedProfilesError());
     }
