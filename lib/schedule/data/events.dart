d598b510b845165710e70a00d9df2bee04ad096b
Alfredo Bautista <alfredobautista1@gmail.com>
2026-02-19 19:33:49 +0100

    include mock values for the conference (#10)

--- lib/schedule/data/events.dart
+++ lib/schedule/data/events.dart
@@ -1,5 +1,13 @@
 import 'package:flutterconf/schedule/schedule.dart';
 
-final allEvents = <Event>[...day1];
+final allEvents = <Event>[...day1, ...day2];
 
-final day1 = <Event>[];
+final day1 = <Event>[
+  ...talks,
+  ...workshops,
+  ...activities.where((a) => a.startTime.day == 10),
+]..sort((a, b) => a.startTime.compareTo(b.startTime));
+
+final day2 = <Event>[
+  ...activities.where((a) => a.startTime.day == 11),
+]..sort((a, b) => a.startTime.compareTo(b.startTime));
